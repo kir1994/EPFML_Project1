f817d85d37b2360fbdc735f96254ccd009aa71d0
kir1994 <antimozgpunk@gmail.com>
2017-10-13 14:11:58 +0200

    Minor fixes

--- code/utilities/pca.py
+++ code/utilities/pca.py
@@ -4,7 +4,7 @@ def compute_pca(x, number_pc=-1):
 	D, N = np.shape(x)
 
 	# Center the data
-	g = (1/K)*x.T.dot(np.eye(D)).dot(np.ones((D, 1))) # center of mass
+	g = (1/D)*x.T.dot(np.eye(D)).dot(np.ones((D, 1))) # center of mass
 	x_c = x - np.ones((D, 1)).dot(g.T)
 	
 	# Spectral analysis
@@ -20,6 +20,9 @@ def compute_pca(x, number_pc=-1):
 		while sum_values/rank < 0.95:
 			sum_values += values[number_pc]
 			number_pc += 1
-	pc = x_c.dot(np.array(vectors[:number_pc])
+	pc = x_c.dot(np.array(vectors[:number_pc]))
+	return pc
+
+
 
 
