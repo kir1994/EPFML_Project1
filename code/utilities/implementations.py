4e2988a86440fcde346c442c53b011ac3c7e959a
Kaeronor <nicolas.talabot@epfl.ch>
2017-10-26 13:11:27 +0200

    New notebook for Kernel (Nico)

--- code/utilities/implementations.py
+++ code/utilities/implementations.py
@@ -62,9 +62,9 @@ def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, \
         # Store w and loss
         ws.append(w)
         losses.append(loss)
-#        if n_iter % 100 == 0:
-#            print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format( \
-#                  bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        if n_iter % 100 == 0:
+            print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format( \
+                  bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
     return ws[-1], losses[-1]
 
 ############################################
